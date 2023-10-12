3cb74e0a51720978d154e59b959692f93e3bc7ee
Yash Mulimani <36557611+imsqsh@users.noreply.github.com>
2023-10-11 18:06:25 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,38 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e) => {
+    
+}
